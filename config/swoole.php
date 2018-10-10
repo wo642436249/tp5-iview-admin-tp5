3b547d64081290370eeeb10155fe7ea4bec172c3
wo642436249 <642436249@qq.com>
2018-10-10 17:04:33 +0800

    Update swoole.php

--- config/swoole.php
+++ config/swoole.php
@@ -20,7 +20,7 @@ return [
     'port' => 9501, // 监听端口
     'mode' => '', // 运行模式 默认为SWOOLE_PROCESS
     'sock_type' => '', // sock type 默认为SWOOLE_SOCK_TCP
-    'app_path' => '/home/wwwroot/www.dahuawulitou.xyz/application/', // 应用地址 如果开启了 'daemonize'=>true 必须设置（使用绝对路径）
+    'app_path' => '/home/wwwroot/xxxxxxx/application/', // 应用地址 如果开启了 'daemonize'=>true 必须设置（使用绝对路径）
     'file_monitor' => false, // 是否开启PHP文件更改监控（调试模式下自动开启）
     'file_monitor_interval' => 2, // 文件变化监控检测时间间隔（秒）
     'file_monitor_path' => [], // 文件监控目录 默认监控application和config目录
